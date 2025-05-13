354e77f7b1df01b8612b3741b84e51468649f84c
Dyari03 <dyariomer03@gmail.com>
2025-05-13 15:50:22 +0300

    Add files via upload

--- PROJECT/confing.php
+++ PROJECT/confing.php
@@ -4,6 +4,9 @@ $dbname = 'library';
 $username = 'root';
 $password = '';
 
+// Corrected mysqli_connect line
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
